18c277b24a5466b1fa23128dea433d8a4407a2b8
Ivan Nulson <ivan.sryvkin@bk.ru>
2023-06-22 20:21:24 +0300

    first commit

---
+++ controllers/PostController.js
@@ -0,0 +1,143 @@
+import PostModel from "../models/Post.js";
+
+export const getLastTags = async (req, res) => {
+  try {
+    const posts = await PostModel.find().sort({ createdAt: -1 }).limit(4).exec();
+		const tags = posts
+		.map((obj) => obj.tag)
+
+    res.json(tags);
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({
+      message: "Не удалось получить тэги",
+    });
+  }
+};
+
+export const getAll = async (req, res) => {
+  try {
+    const posts = await PostModel.find()
+      .sort({ createdAt: -1 })
+      .populate("user")
+      .exec();
+    res.json(posts);
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({
+      message: "Не удалось получить статьи",
+    });
+  }
+};
+
+export const getOne = async (req, res) => {
+  try {
+    const postId = req.params.id;
+
+    PostModel.findOneAndUpdate(
+      {
+        _id: postId,
+      },
+      {
+        $inc: { viewsCount: 1 },
+      },
+      {
+        returnDocument: "after",
+      }
+    ).then((doc, err) => {
+      if (err) {
+        return res.status(500).json({ message: "Не удалось получить статью" });
+      }
+      if (!doc) {
+        return res.status(404).json({ message: "Статья не найдена" });
+      }
+      res.json(doc);
+    });
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({
+      message: "Не удалось получить статьи",
+    });
+  }
+};
+
+export const remove = async (req, res) => {
+  try {
+    const postId = req.params.id;
+
+    PostModel.findOneAndDelete(
+      {
+        _id: postId,
+      },
+      (err, doc) => {
+        if (err) {
+          console.log(err);
+          return res.status(500).json({
+            message: "Не удалось удалить статью",
+          });
+        }
+
+        if (!doc) {
+          return res.status(404).json({
+            message: "Статья не найдена",
+          });
+        }
+
+        res.json({
+          success: true,
+        });
+      }
+    );
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({
+      message: "Не удалось получить статьи",
+    });
+  }
+};
+
+export const create = async (req, res) => {
+  try {
+    const doc = new PostModel({
+      title: req.body.title,
+      text: req.body.text,
+      imgUrl: req.body.imgUrl,
+      tag: req.body.tag,
+      user: req.userId,
+    });
+
+    const post = await doc.save();
+    res.json(post);
+  } catch (err) {
+    res.status(500).json({
+      message: "Не удалось создать статью",
+    });
+  }
+};
+
+export const update = async (req, res) => {
+  try {
+    const postId = req.params.id;
+
+    await PostModel.updateOne(
+      {
+        _id: postId,
+      },
+      {
+        title: req.body.title,
+        text: req.body.text,
+        imgUrl: req.body.imgUrl,
+        tag: req.body.tag,
+        user: req.userId,
+      }
+    );
+    res.json({
+      success: true,
+    });
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({
+      message: "Не удалось обновить статью",
+    });
+  }
+};
